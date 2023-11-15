929f3cbb58d598fc1606b5e2d2fc288d80e132a5
Oliver <oliver.brito22@gmail.com>
2023-11-14 20:50:18 -0500

    implemented user routes/functionality and db connection

---
+++ src/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+   
