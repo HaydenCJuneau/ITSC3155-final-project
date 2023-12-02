c1250250993b11236dc6ef73ae92f3c16f7f503b
Michael Gohn <michaelgohn03@gmail.com>
2023-12-02 14:30:25 -0500

    Updated database_schema.sql with new columns

--- database_schema.sql
+++ database_schema.sql
@@ -4,29 +4,33 @@ CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(256) NOT NULL,
-); 
+    password VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS posts (
     post_id SERIAL PRIMARY KEY,
-    author_id INT REFERENCES users(user_id),
-    postNum_likes INT,
-    timestamp TIMESTAMP,
-    caption VARCHAR(200)
+    image BYTEA NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    description TEXT,
+    status VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY(author_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments (
     comment_id SERIAL PRIMARY KEY,
-    post_id INT REFERENCES posts(post_id),
-    user_id INT REFERENCES users(user_id)
-    text TEXT,
-    commentNum_likes INT
+    text TEXT NOT NULL,
+    post_id INT NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY(post_id) REFERENCES posts(post_id),
+    FOREIGN KEY(author_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS likes (
    like_id SERIAL PRIMARY KEY,
-   user_id INT REFERENCES users(user_id),
-   post_id INT REFERENCES posts(post_id),
-   comment_id  INT REFERENCES comments(comment_id),
-   liked_at TIMESTAMP
-   );
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+   FOREIGN KEY(user_id) REFERENCES users(user_id),
+   FOREIGN KEY(post_id) REFERENCES posts(post_id)
+);
