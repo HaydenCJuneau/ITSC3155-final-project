557272fa5c1aae4caa6a6820d013e504823ab0c4
yadhira <yadhiramarcosavila@gmail.com>
2023-12-02 12:22:26 -0500

    adding posts, comments, and likes table

--- database_schema.sql
+++ database_schema.sql
@@ -5,4 +5,28 @@ CREATE TABLE user (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(256) NOT NULL,
+); 
+
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    author_id INT REFERENCES user(user_id),
+    postNum_likes INT,
+    timestamp TIMESTAMP,
+    caption VARCHAR(200)
 );
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id),
+    user_id INT REFERENCES user(user_id)
+    text TEXT,
+    commentNum_likes INT
+);
+
+CREATE TABLE likes (
+   like_id SERIAL PRIMARY KEY,
+   user_id INT REFERENCES user(user_id),
+   post_id INT REFERENCES posts(post_id),
+   comment_id  INT REFERENCES comments(comment_id),
+   liked_at TIMESTAMP
+   );
