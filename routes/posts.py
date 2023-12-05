f9bb55fbcf1322dab9bbe7c02ce1eca2bda018a1
Hayden <66452827+HaydenCJuneau@users.noreply.github.com>
2023-12-05 18:17:26 -0500

    Open up backend route for post creation

--- routes/posts.py
+++ routes/posts.py
@@ -1,12 +1,33 @@
 # src/posts.py
 # This file holds API routes for post functionality and nav
 from . import posts_bp
+import os
 from flask import render_template, request
+from src.post_utils import queue_image_generation, encode_ctrl_image, decode_image
+
+TOKEN = os.getenv('API_KEY')
+
 
 
 @posts_bp.post('/posts/generate')
-def generate_image():
-    pass
+def generate_post():
+    """
+    Data will hold all information about the new post
+    title - The title of the new post
+    description - the description/prompt for the post
+    image - a blob or b64 encoded image
+    """
+    data = request.get_json()
+    print(data)    
+    
+    # TODO: Create a post in the DB, flagged as pending
+
+    # TODO: Start an async call to the gen API
+    generated = queue_image_generation(0, data['description'], data['image'])
+
+    print(f'Generated an image!')
+    
+    return { "response": 200, "image": generated }
 
 
 @posts_bp.get('/new_post')
