becc6e403e1371e81466bdd26c65beb1127704ba
Michael Gohn <michaelgohn03@gmail.com>
2023-12-05 19:02:29 -0500

    added reprs to models. fixed a few typos

--- src/models.py
+++ src/models.py
@@ -10,6 +10,9 @@ class Users(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
+    def __repr__(self) -> str:
+        return f'Users({self.user_id}, {self.username}, {self.email}, {self.password})'
+
 class Posts(db.Model):
     __tablename__ = 'posts'
     
@@ -18,11 +21,14 @@ class Posts(db.Model):
     title = db.Column(db.String(255), nullable=False)
     timestamp = db.Column(db.TIMESTAMP, nullable=False)
     description = db.Column(db.Text, nullable=False)
-    status = db.Column(db.String(255), nullabe=False)
+    status = db.Column(db.String(255), nullable=False)
 
-    author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), \
+    author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), \
                             nullable=False)
     author = db.relationship('Users', backref='posts')
+
+    def __repr__(self) -> str:
+        return f'Posts({self.post_id}, {self.image}, {self.title}, {self.timestamp}, {self.description}, {self.status}, {self.author_id})'
 
 class Comments(db.Model):
     __tablename__ = 'comments'
@@ -30,13 +36,16 @@ class Comments(db.Model):
     comment_id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
 
-    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), \
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), \
                         nullable=False)
     post = db.relationship('Posts', backref='comments')
 
-    author_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), \
+    author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), \
                         nullable=False)
     author = db.relationship('Users', backref='comments')
+
+    def __repr__(self) -> str:
+        return f'Comments({self.comment_id}, {self.text}, {self.post_id}, {self.author_id}'
 
 class Likes(db.Model):
     __tablename__ = 'likes'
@@ -49,3 +58,6 @@ class Likes(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), \
                         nullable=False)
     post = db.relationship('Posts', backref='likes')
+
+    def __repr__(self) -> str:
+        return f'Likes({self.like_id}, {self.user_id}, {self.post_id}'
