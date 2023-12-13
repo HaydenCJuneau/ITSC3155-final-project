c0f79736b342564f3267a5169c9f8fbea3474af6
Michael Gohn <michaelgohn03@gmail.com>
2023-12-12 20:43:41 -0500

    Simple change nothing major

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def profile():
     user_id = session['user_id']
     user = get_user_by_id(user_id)
     # Temp DELETE THIS!!!
-    dummy_image_path = 'static/images/hayden_headshot.JPG'
+    dummy_image_path = 'static/images/michael_headshot.JPG'
     dummy_image_binary = open(dummy_image_path, 'rb').read()
     post1 = Posts(image=dummy_image_binary, title='title1', timestamp=datetime.utcnow(), description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate saepe earum facere aut ipsum perspiciatis magni vitae aliquid nihil pariatur? Repellat, officia magnam excepturi rem cumque error laudantium harum numquam maiores maxime nulla fugiat, hic est eum quibusdam culpa ducimus. Saepe at dolore soluta cumque sequi! Deleniti magnam iste temporibus non. Consequuntur possimus voluptatum ab natus praesentium ea laboriosam rerum. Corrupti obcaecati quos id ipsam cupiditate excepturi quasi perspiciatis voluptatum optio vero, officia quis nam qui soluta doloribus minus. Laudantium dolorem nam aspernatur voluptatum esse ullam et iusto excepturi. Excepturi exercitationem ut fugit, cumque qui, adipisci reprehenderit recusandae distinctio, laborum nihil alias explicabo magni itaque molestias officiis voluptatum voluptatibus vel iusto voluptates? Eveniet, animi quo distinctio impedit maxime fugit? Blanditiis aut distinctio exercitationem? Nobis distinctio iste rem, vel maiores quisquam, qui fugit corporis debitis ab explicabo nostrum natus!', status='GOOD', author_id=user.user_id)
     save_item_to_db(post1)
